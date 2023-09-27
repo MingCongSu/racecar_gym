0cf7e885703c40f6e6c11b0bd7674d8b00edd094
MingCongSu <m11152014@mail.ntust.edu.tw>
2023-09-27 13:26:30 +0800

    remove pettingzoo api & update __init__.py

--- racecar_gym/envs/__init__.py
+++ racecar_gym/envs/__init__.py
@@ -1,5 +1,4 @@
 from . import gym_api
-from . import pettingzoo_api
 from .scenarios import MultiAgentScenario, SingleAgentScenario
 
-__all__ = ["pettingzoo_api", "gym_api", "MultiAgentScenario", "SingleAgentScenario"]
+__all__ = ["gym_api", "MultiAgentScenario", "SingleAgentScenario"]
--- racecar_gym/envs/pettingzoo_api/__init__.py
+++
@@ -1,4 +0,0 @@
-from .env_factories import *
-
-
-__all__ = ["env", "raw_env"]
